8573617cddaf069721df7d52f4da3ee8dbedef24
sozan45 <sozan45@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-14 10:24:00 +0000

    upload main
    git-svn-id: https://plugins.svn.wordpress.org/ultra-mouse-tail/trunk@1025638 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,163 @@
+<?php
+/*
+	Plugin Name: Ultra Mouse-Tail
+	Plugin URI: http://www.themeultra.com/ultra-mouse-tail-demo/
+	Description: This is a Awesome Mouse Tail Plugin. By using this Plugin You can make your website more gorgeous.
+	Author: Khurshid Alam Mojumder
+	Version: 1.0
+	Author URI: http://www.themeultra.com
+*/
+
+
+
+/* Enqueue Java Script */
+function ultra_jquery_main_js() {
+	wp_enqueue_script('jquery');;
+}
+add_action('init','ultra_jquery_main_js');
+
+
+/* Enqueue Java Script */
+function ultra_mousetail_main_js() {
+	wp_enqueue_script( 'ultra-mousetail-js', plugins_url( '/js/ultra_mousetail.js', __FILE__ ), array('jquery'), false);;
+}
+add_action('wp_footer','ultra_mousetail_main_js');
+
+
+function ultra_mouse_tail_manu(){
+
+	 add_options_page( 'Ultra Mouse Tail', 'Ultra Mouse Tail', 'manage_options', 'mouse-tail-page', 'ultea_mouse_tail_options_page_function', plugins_url( 'myplugin/images/icon.png' ),8 );
+
+			 
+}
+add_action('admin_menu','ultra_mouse_tail_manu');
+		  
+// 2. Add default value array. 
+$ultra_mouse_tail_options_default = array(
+	'ultea_mouse_text' => "Stay With Us",
+);
+
+ 
+
+    if ( is_admin() ) : // 3. Load only if we are viewing an admin page	
+
+  // 4. Add setting option by used function. 
+function ultea_mouse_tail_register_settings() {
+// Register settings and call sanitation functions
+// 4. Add register setting. 
+	register_setting( 'ultea_mouse_tail_p_options', 'ultra_mouse_tail_options_default', 'ultea_mouse_tail_validate_options' );
+}
+
+add_action( 'admin_init', 'ultea_mouse_tail_register_settings' );
+
+
+
+ //1.2
+ function ultea_mouse_tail_options_page_function(){?>
+ 
+ <?php // 5.1. Add settings API hook under form action.  ?>
+<?php global $ultra_mouse_tail_options_default ;
+
+
+if ( ! isset( $_REQUEST['updated'] ) )
+	$_REQUEST['updated'] = false; // This checks whether the form has just been submitted. 
+
+?>
+	 
+	 
+   <div class="wrap">
+	  <h2>Cursor Text setting</h2>
+				  <?php if ( false !== $_REQUEST['updated'] ) : ?>
+		<div class="updated fade"><p><strong><?php _e( 'Options saved' ); ?></strong></p></div>
+		<?php endif; // 5.2. If the form has just been submitted, this shows the notification ?>	
+	  
+	<form action="options.php" method="post">  
+	  
+	  
+
+	<?php // 6.1 Add settings API hook under form action.  ?>
+<?php $settings = get_option( 'ultra_mouse_tail_options_default', $ultra_mouse_tail_options_default ); ?>
+
+
+<?php settings_fields( 'ultea_mouse_tail_p_options' );
+/* 6.2  This function outputs some hidden fields required by the form,
+including a nonce, a unique number used to ensure the form has been submitted from the admin page and not somewhere else, very important for security */ ?>
+
+
+
+		
+	<table class="form-table">
+		<tbody>
+			<tr valign="top">
+				<th scope="row"><label for="ultea_mouse_text">Ultra MouseTail Cursor Text:</label></th>
+					<td>
+						<input type="text" class="" value="<?php echo stripslashes($settings['ultea_mouse_text']); ?>" id="ultea_mouse_text" name="ultra_mouse_tail_options_default[ultea_mouse_text]"/><p class="description">Please Type The text Which you want with cursor</p>
+					</td>
+			</tr>
+		
+	</tbody>
+
+</table>
+
+
+<p class="submit">
+<input type="submit" value="Save Changes" class="button button-primary" id="submit" name="submit">
+</p>
+
+</form>
+			   
+</div>
+	 
+	 
+	<?php 
+	 }
+	 
+
+  // 7. Add validate options. 
+function ultea_mouse_tail_validate_options( $input ) {
+	global $ultra_mouse_tail_options_default;
+
+	$settings = get_option( 'ultra_mouse_tail_options_default', $ultra_mouse_tail_options_default );
+	
+	
+	$input['ultea_mouse_text']=wp_filter_post_kses($input['ultea_mouse_text']);
+	
+	
+	 // We strip all tags from the text field, to avoid vulnerablilties like XSS
+	$prev=$settings['layout_only'];
+	if(!array_key_exists($input['layout_only']))
+	$input['layout_only']=$prev;
+	
+
+ return $input;
+}
+
+
+ 
+	endif;  //3. EndIf is_admin()	
+
+
+// 8.data danamic
+		
+function ultea_mouse_tail_activator(){?>
+<?php global $ultra_mouse_tail_options_default;
+
+$ultra_mouse_tail_settings=get_option('ultra_mouse_tail_options_default','$ultra_mouse_tail_options_default'); ?>
+
+	<!--use this where need dynamic data-->
+	<?php // echo $ultra_mouse_tail_settings['ultea_mouse_text']; ?>
+	<script type='text/javascript'>
+		var text="<?php echo $ultra_mouse_tail_settings['ultea_mouse_text']; ?>";
+	</script>	
+		
+	
+	
+<?php
+}
+
+add_action('wp_footer','ultea_mouse_tail_activator');
+
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== Ultra Mouse Tail ===
+Tags: Mousetail, jQuery Mousetail, Awesome Mousetail, jQuery Mouse effect, WP Mousetail Plugin, Mouse Text, Ultra Mouse Tail, Mouse Animation, Cursor Text, wordpress cursor Text, Cursor Animation 
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+This Plugin will enable Real-Time Cursor Text in your WordPress Theme. 
+
+== Installation ==
+
+1. Upload `plugin-directory` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. After Activate "Ultra Camera Slider", U Can See a new menu "Ultra Mouse Tail" in Settings Menu on your WordPress Dashboard. 
+6. If you want to change the cursor text need to go "Ultra Mouse Tail" in Settings Menu on your WordPress Dashboard than change the text and click "Save Change" button.
+
+
+
+== Frequently Asked Questions ==
+
+
+
+== Screenshots ==
+1. Ultra Setting Menu in WordPress Dashboard Menu.
+2. Cursor Text Setting Menu.
+3. Ultra Mouse Tail Plugin in WordPress Plugin Directory.
+4. It's Awesome cursor Effect in Your Website.
+
+== Changelog ==
+
+= 1.0 =
+* Initial Release
